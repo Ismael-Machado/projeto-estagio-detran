fede439f27445ca00f9cdcb274406bd09c8df0cf
Ismael-Machado <ismael.ms2010@gmail.com>
2023-07-20 22:02:43 -0500

    formulário de login

--- Project/src/views/login.php
+++ Project/src/views/login.php
@@ -11,6 +11,33 @@
     <title>Chamados Detran</title>
 </head>
 <body>
-    <h1>Login</h1>
+    <!-- <figure class="img-top"> 
+        <img src="https://www.detran.ac.gov.br/wp-content/uploads/2023/01/logoSiteDetranSecom.png" alt="logo do governo do acre e logo do detran" class="img-logo">
+    </figure> -->
+    
+    <form class="form-login" action="#" method="post">
+        <div class="login-card card">
+            <div class="card-header">
+                <i class="icofont-megaphone mr-2"></i>
+                <span class="font-weight-bold">CHAMADOS</span>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email" id="email">E-mail</label>
+                    <input type="email" id="email" name="email" class="form-control"
+                    placeholder="Informe seu e-mail institucional" autofocus> 
+                </div>
+                <div class="form-group">
+                    <label for="password" id="password">Senha</label>
+                    <input type="password" id="password" name="password" class="form-control"
+                    placeholder="Informe a sua senha"> 
+                    <a href="#" class="restaurar-senha mx-1">restaurar senha?</a>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-success">Entrar</button>
+            </div>
+        </div>
+    </form>
 </body>
 </html>
