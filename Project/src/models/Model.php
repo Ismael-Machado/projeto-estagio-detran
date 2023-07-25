ff556edfe86dd9f42f274fb15b5d8220a83be333
Ismael Machado <ismael.silva@sou.ufac.br>
2023-07-25 17:08:00 -0500

    get e set da classe Model

--- Project/src/models/Model.php
+++ Project/src/models/Model.php
@@ -2,6 +2,29 @@
 
 //classe que serve de base (modelo/model) para interação com o banco de dados 
 class Model {
+    protected static $tableName = '';
+    protected static $columns = [];
+    protected $values = [];
+    
+    function __construct($array) {
+        $this->loadFromArray($array);
+    }
 
     
+    public function loadFromArray($array) {
+        if($array){
+            foreach($array as $key => $value) {
+                $this->set($key, $value);
+            }
+        }
+    }
+
+    public function get($key) {
+
+    }
+
+
+    public function set($key, $value) {
+        $this->values[$key] = $value;
+    }
 }
--- Project/src/models/User.php
+++ Project/src/models/User.php
@@ -2,6 +2,15 @@
 
 
 class User extends Model {
-    
+    protected static $tableName = 'usuarios';
+    protected static $columns = [
+        'usuario_id',
+        'usuario_nome',
+        'usuario_senha',
+        'usuario_email',
+        'usuario_criado_em',
+        'usuario_is_ativo',
+        'usuario_is_admin',
+    ];
     
 }
