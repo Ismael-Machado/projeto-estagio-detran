dc554cb9dc2a36232856573f8b7ed8541860313e
Ismael-Machado <ismael.ms2010@gmail.com>
2023-07-28 21:51:57 -0500

    parse_url: php_url_path no index

--- Project/public/index.php
+++ Project/public/index.php
@@ -4,7 +4,7 @@
 //os requires necessários para toda a aplicação 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI']), PHP_URL_PATH);
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if($uri === '/' || $uri === '' || $uri === '/index.php') {
     $uri = 'login.php';
