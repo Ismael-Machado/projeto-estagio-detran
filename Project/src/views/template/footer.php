e45967a08f1bf7d9b306a6c77783c4e9ba886a1d
Ismael Machado <ismael.silva@sou.ufac.br>
2023-08-10 12:39:41 -0500

    footer e menu lateral: estilizações pelo template.css

--- Project/src/views/template/footer.php
+++ Project/src/views/template/footer.php
@@ -1,5 +1,12 @@
 <footer class="footer">
-    <span>Sistemas de chamados</span>
+    <figure class="img-footer"> 
+        <img src="/assets/img/logoGovernoAc.png" alt="logo do governo do acre" class="img-logo-footer-governo">
+    </figure>
+    <span class="text-footer">Sistema de Gestão de Chamados - Divisão de Tecnologia da Informação</span>
+    <figure class="img-footer"> 
+        <img src="/assets/img/logoDetranAc.png" alt="logo do detran acre" class="img-logo-footer-detran">
+    </figure>
 </footer>
+<script src="assets/js/script.js"></script>
 </body>
 </html>
