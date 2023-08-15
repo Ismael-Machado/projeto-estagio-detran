6d1628029778e5a03a3ee4356b83cdfb4abad3a0
Ismael Machado <ismael.silva@sou.ufac.br>
2023-08-15 12:32:00 -0500

    form chamados: alteração na estrutura da view de chamados

--- Project/src/views/chamados.php
+++ Project/src/views/chamados.php
@@ -12,19 +12,64 @@
             <p class="mb-0">Preencha os dados corretamente</p>
         </div>
         <div class="card-body">
-            <div class="d-flex m-5 justify-content-around">
-                <span>Nome</span>
-                <span>Matrícula</span>
-            </div>
-            <div class="d-flex m-5 justify-content-around">
-                <span>Setor</span>
-                <span>Problema</span>
-            </div>
-            <div class="d-flex m-5 justify-content-around">
-                <span>Decrição</span>
-            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="nome">Nome</label>
+                    <input type="text" id="nome" name="chamado_solicitante" placeholder="Informe seu nome" class="form-control <?= $errors[''] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $errors[''] ?>
+                    </div>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="email">E-mail</label>
+                    <input type="text" id="email" name="chamado_email_solicitante" placeholder="Informe seu e-mail" class="form-control <?= $errors[''] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $errors[''] ?>
+                    </div>
+                </div>
+            </div>    
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="setor">Setor</label>
+                    <select class="form-select form-control" aria-label="Default select example">
+                        <option selected>Selecione o setor</option>
+                        <option value="1">Administração</option>
+                        <option value="2">Jurídico</option>
+                        <option value="3">Atendimento</option>
+                    </select>
+                    <div class="invalid-feedback">
+                        <?= $errors[''] ?>
+                    </div>
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="email">Problema</label>
+                    <select class="form-select form-control" aria-label="Default select example">
+                        <option selected>Selecione a categoria do problema</option>
+                        <option value="1">Internet</option>
+                        <option value="2">Monitor</option>
+                        <option value="3">Outro</option>
+                    </select>
+                    <div class="invalid-feedback">
+                        <?= $errors[''] ?>
+                    </div>
+                </div>
+            </div>    
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="descricao">Descrição</label>
+                    <input type="text" id="descricao" name="chamado_email_solicitante" 
+                    placeholder="Forneça mais detalhes do problema, se desejar" class="form-control <?= $errors[''] ? 'is-invalid' : '' ?>">
+                    <div class="invalid-feedback">
+                        <?= $errors[''] ?>
+                    </div>
+                </div>
+            </div>    
         </div>
         <div class="card-footer d-flex justify-content-center">
+            <!-- se todos os dados passarem pela validação, o href desse butão link
+            chama o controller de confirmação de inserção e carrega na view a 
+            confirmação de abertura do chamado com o número do chamado para 
+            posterior pesquisa -->
             <a href="???" class="btn btn-success btn-lg">
                 <i class="icofont-check mr-1"></i>
                 Confirma
