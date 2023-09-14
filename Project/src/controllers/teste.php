b9092214610ffc88bba7eb71e0c75459946c3541
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-09-13 19:47:26 -0500

    fix(teste): Resolve conflito no arquivo de teste
    Solução de conflitos no arquivo "teste.php"

--- Project/src/controllers/teste.php
+++ Project/src/controllers/teste.php
@@ -8,7 +8,7 @@ $totalChamados = Chamados::getTotalChamados();
 print_r($totalChamados);
 
 echo("<br>");
-echo("Testando função para retornar o primerio dia útil do mês.");
+echo("Testando função para retornar o primeiro dia útil do mês.");
 echo("<br>");
 print_r(getFirstDayOfMonth('2023-02-10'));
 echo("<br>");
