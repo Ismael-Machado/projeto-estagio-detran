859694b912716aec7b28d3c4149c224df8300af7
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2023-09-15 14:28:17 -0500

    style(filtro): Adiciona tabela com estilo básico
    Tabela exibindo os dados do mês atual

--- Project/src/views/filtro_chamados.php
+++ Project/src/views/filtro_chamados.php
@@ -5,12 +5,33 @@
         'Filtro dos chamados por mês e status',
         'icofont-search-stock'
     );
-    print_r($registries);
-
-    echo "<br><br>";
-    $dates = array_keys($registries);
-    foreach ($dates as $date) {
-        echo "Data: $date<br>";
-    }
     ?>
+    <div>
+        <table class="table table-bordered table-striped table-hover">
+            <thead>
+                <th>Solicitante</th>
+                <th>Email</th>
+                <th>Assunto</th>
+                <th>Descrição</th>
+                <th>Setor</th>
+                <th>Data de Solicitação</th>
+                <th>Status</th>
+                <th>Ações</th>
+            </thead>
+            <tbody>
+                <?php foreach ($registries as $registry): ?>
+                    <tr>
+                    <td><?= $registry->chamado_solicitante ?></td>
+                    <td><?= $registry->chamado_email_solicitante ?></td>
+                    <td><?= $registry->chamado_assunto ?></td>
+                    <td><?= $registry->chamado_descricao ?></td>
+                    <td><?= $registry->chamado_setor ?></td>
+                    <td><?= $registry->chamado_criado_em ?></td>
+                    <td><?= $registry->chamado_status ?></td>
+                    <td>Adicionar botões</td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
 </main>
