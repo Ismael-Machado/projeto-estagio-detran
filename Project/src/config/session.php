69cc3c5c37a158c6a6441fc41ec5171f301ee2b2
Ismael Machado <ismael.silva@sou.ufac.br>
2023-12-26 14:05:12 -0500

    update em session

--- Project/src/config/session.php
+++ Project/src/config/session.php
@@ -1,9 +1,13 @@
 <?php 
 
-function requireValidSession() {
+function requireValidSession($requiresAdmin = false) {
     $user = $_SESSION['user'];
     if(!isset($user)) {
         header('Location: login.php');
+        exit();
+    } elseif($requiresAdmin && !$user->usuario_is_admin) {
+        addErrorMsg('Acesso negado!');
+        header('Location: chamados.php');
         exit();
     }
 }
